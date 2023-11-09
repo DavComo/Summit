b623313a9d1d9ee700cc61da9c3dadda8acdd5a0
frfr <chookitypok123@gmail.com>
2023-11-09 15:19:16 +0100

    dear changemaker 2

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ function toggleResponsive() {
   var x = document.getElementById("topNav");
   if (x.className === "topnav") {
     x.className += " responsive";
-    document.getElementById("topNav").style = "height: 300px; z-index: 50;";
+    document.getElementById("topNav").style = "height: 300px;";
   } else {
     x.className = "topnav";
-    document.getElementById("topNav").style = "height: 60px; z-index: 50;";
+    document.getElementById("topNav").style = "height: 60px;";
   }
 }
 
 document.addEventListener("scroll", () => {
   if (document.body.scrollTop >= 50) {
-    document.getElementById("topNav").style = "height: 60px; z-index: 50";
+    document.getElementById("topNav").style = "height: 60px;";
   } else {
     document.getElementById("topNav").style = "height: 300px;"
   }
