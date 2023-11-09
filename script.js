151b45ce095bd87eff26950584f2c7567cda50bb
Kelsey-Wainwright <kelseywainwright09@gmail.com>
2023-11-09 09:05:12 +0100

    changes

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ document.addEventListener("scroll", () => {
   if (document.body.scrollTop >= 50) {
     document.getElementById("topNav").style = "height: 60px; z-index: 50";
   } else {
-    document.getElementById("topNav").style = "height: 350px;"
+    document.getElementById("topNav").style = "height: 300px;"
   }
 });
